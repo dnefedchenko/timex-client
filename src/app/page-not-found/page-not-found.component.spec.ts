fc0640ec02bb0963131dbf5f9321a7343768b2a5
Nefedchenko, Dmitriy <dmitriy.nefedchenko@sigma.software>
2019-07-10 16:39:52 +0300

    Page not found component unit test fixed

--- src/app/page-not-found/page-not-found.component.spec.ts
+++ src/app/page-not-found/page-not-found.component.spec.ts
@@ -1,13 +1,15 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PageNotFoundComponent } from './page-not-found.component';
+import {RouterTestingModule} from '@angular/router/testing';
 
-xdescribe('PageNotFoundComponent', () => {
+describe('PageNotFoundComponent', () => {
   let component: PageNotFoundComponent;
   let fixture: ComponentFixture<PageNotFoundComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
       declarations: [ PageNotFoundComponent ]
     })
     .compileComponents();
