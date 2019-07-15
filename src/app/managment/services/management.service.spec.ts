4ca88babdb570ad3704b56487d982c274f0984bf
Nefedchenko, Dmitriy <dmitriy.nefedchenko@sigma.software>
2019-07-15 14:37:48 +0300

    ManagementService unit test spec added

--- src/app/managment/services/management.service.spec.ts
+++ src/app/managment/services/management.service.spec.ts
@@ -1,3 +1,61 @@
-describe('ManagementService Unit Test', () => {
+import {staffHourReports, timesheet} from '../../app.constants';
+import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
+import {environment} from '../../../environments/environment';
+import {Timesheet} from '../../model/employee/timesheet.interface';
+import {TestBed} from '@angular/core/testing';
+import {HttpClient} from '@angular/common/http';
+import {ManagementService} from './management.service';
+import {StaffHoursReport} from '../../model/manager/staff-hours.report.interface';
 
+describe('ManagementService Unit Test', () => {
+  let testee: ManagementService;
+
+  let apiUrl;
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
+
+  const reports: StaffHoursReport[] = staffHourReports;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [
+        ManagementService,
+        { provide: 'apiUrl', useValue: environment.apiUrl }
+      ]
+    });
+
+    testee = TestBed.get(ManagementService);
+    apiUrl = TestBed.get('apiUrl');
+    httpClient = TestBed.get(HttpClient);
+    httpTestingController = TestBed.get(HttpTestingController);
+  });
+
+  it('should get employee timesheets', () => {
+    testee
+      .loadCurrentWeekReports()
+      .subscribe((response: StaffHoursReport[]) => {
+        expect(response).toEqual(reports);
+      });
+
+    const performedRequest = httpTestingController.expectOne(`${apiUrl}/current-week-reports`);
+    expect(performedRequest.request.method).toEqual('GET');
+    performedRequest.flush(reports);
+  });
+
+  it('should approve time reports', () => {
+    testee
+      .approveTimesheets(staffHourReports)
+      .subscribe(response => {
+        expect(response).toBeDefined();
+      });
+
+    const performedRequest = httpTestingController.expectOne(`${apiUrl}/timesheets/approve`);
+    expect(performedRequest.request.method).toEqual('POST');
+    performedRequest.flush(staffHourReports);
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
+  });
 });
--- src/app/managment/services/management.service.ts
+++ src/app/managment/services/management.service.ts
@@ -11,12 +11,12 @@ export class ManagementService {
   constructor(@Inject('apiUrl') private apiUrl: string, private httpClient: HttpClient) {}
 
   public loadCurrentWeekReports(): Observable<StaffHoursReport[]> {
-    return of(staffHourReports);
-//    return this.httpClient.get(`${this.apiUrl}/current-week-reports`);
+//    return of(staffHourReports);
+    return this.httpClient.get<StaffHoursReport[]>(`${this.apiUrl}/current-week-reports`);
   }
 
   public approveTimesheets(reports: StaffHoursReport[]): Observable<any> {
-    return of(reports);
-//    return this.httpClient.post(`${this.apiUrl}/timesheets/approve`, reports);
+//    return of(reports);
+    return this.httpClient.post(`${this.apiUrl}/timesheets/approve`, reports);
   }
 }
